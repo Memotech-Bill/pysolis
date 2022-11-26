7ecd9bcc7f2195820cfb335b27b3263a85a77a63
Memotech Bill <64212746+Memotech-Bill@users.noreply.github.com>
2022-11-26 19:45:36 +0000

    Revisions to HTML styles

--- solis_query.py
+++ solis_query.py
@@ -13,26 +13,41 @@ def query (addr, logdir):
     registers = ((33022, 33041), (33049, 33059), (33071, 33085), (33091, 33096),
                  (33100, 33107), (33126, 33151), (33161, 33181), (33251, 33287),
                  (33115, 33122))
-    rec = b'\xAA\x55'
-    rec += struct.pack ('<Q', int (time.time ()))
-    sock = socket.create_connection ((addr, 30003), timeout = 20)
-    for fst, lst in registers:
-        req = umodbus.client.tcp.read_input_registers (sid, fst, lst - fst)
-        bFail = True
-        for attempt in range (3):
-            try:
-                val = umodbus.client.tcp.send_message (req, sock)
-                bFail = False
-                break
-            except TimeoutError:
-                pass
-        if ( bFail ):
+    nTry = 0
+    while ( nTry < 2 ):
+        nTry += 1
+        try:
+            fst = 0
+            lst = 0
+            rec = b'\xAA\x55'
+            rec += struct.pack ('<Q', int (time.time ()))
+            sock = socket.create_connection ((addr, 30003), timeout = 20)
+            for fst, lst in registers:
+                req = umodbus.client.tcp.read_input_registers (sid, fst, lst - fst)
+                bFail = True
+                for attempt in range (3):
+                    try:
+                        val = umodbus.client.tcp.send_message (req, sock)
+                        bFail = False
+                        break
+                    except TimeoutError:
+                        pass
+                if ( bFail ):
+                    sock.close ()
+                    sys.stderr.write ('Failed to read registers ({:d}, {:d})\n'.format (fst, lst))
+                    sys.exit (1)
+                for v in val:
+                    rec += struct.pack ('<H', v)
             sock.close ()
-            sys.stderr.write ('Failed to read registers ({:d}, {:d})\n'.format (fst, lst))
-            sys.exit (1)
-        for v in val:
-            rec += struct.pack ('<H', v)
-    sock.close ()
+            break
+        except ConnectionResetError:
+            if ( fst == 0 ):
+                sys.stderr.write ('{:s} Try {:d} - Connection reset on open\n'
+                                  .format (time.strftime ('%Y-%m-%d %H:%M:%S'), nTry))
+            else:
+                sys.stderr.write ('{:s} Try {:d} - Connection reset reading registers ({:d{, {:d})\n'
+                                  .format (time.strftime ('%Y-%m-%d %H:%M:%S'), nTry, fst, lst))
+            time.sleep (120)
     rec += struct.pack ('<Q', int (time.time ()))
     rec += b'\x55\xAA'
     tm = time.gmtime (time.time ())
